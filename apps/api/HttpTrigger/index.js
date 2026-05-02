38d31a6f0774a532b166dbf5a997dcf76eb45f9e
Harsh <harshjhunjhunwala0628@gmail.com>
2026-05-02 15:08:02 +0530

    feat(api): add Azure Functions HTTP trigger files

---
+++ apps/api/HttpTrigger/index.js
@@ -0,0 +1,14 @@
+﻿const serverlessExpress = require("@codegenie/serverless-express");
+const app = require("../src/app");
+const connectDb = require("../src/config/db");
+
+let dbReady = false;
+const expressHandler = serverlessExpress({ app });
+
+module.exports = async function (context, req) {
+  if (!dbReady) {
+    await connectDb();
+    dbReady = true;
+  }
+  return expressHandler(context, req);
+};
